a07dcbb432325fa82d604c2e0eedfe6211d33d12
fahrettin-Kasa <143270684+fahrettin-Kasa@users.noreply.github.com>
2024-12-13 17:02:30 +0300

    Next15_App

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import BlogList from "@/Components/BlogList";
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <BlogList/>
+      <Footer/>
+    </>
+  );
+}
